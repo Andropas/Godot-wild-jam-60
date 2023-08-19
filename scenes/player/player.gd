b379aabdc771bfd976952b89196cbfd8cab618f5
Andropas <mr.aagaron@gmail.com>
2023-08-19 16:08:35 +0300

    Player now can punch you :D

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -9,8 +9,10 @@ signal changed_facing(x_value: int)
 var hit_points = 4
 @onready var stun_timer = $StunTimer
 @onready var safe_timer = $SafeTimer
+@onready var punch_timer = $PunchTimer
 @onready var effect_anim = $EffectAnimationPlayer
 @onready var anim_tree = $AnimationTree
+@onready var anim_player = $AnimationPlayer
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var jump_timer = $JumpTimer
@@ -29,6 +31,8 @@ func take_damage(dmg, kickback = -1, stun = 0):
 		stun_timer.start(stun)
 		effect_anim.play("hit")
 		safe_timer.start(1.5)
+		punch_timer.stop()
+		$PunchArea/CollisionShape2D.set_deferred("disabled", true)
 		emit_signal("changed_hp", hit_points)
 		#print(hit_points)
 		if hit_points <= 0:
@@ -40,19 +44,24 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	if not stun_timer.time_left:
+	if not stun_timer.time_left and not punch_timer.time_left:
 		
 		# Handle Jump.
+		
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			jump_timer.start()
 		if Input.is_action_just_released("jump"):
 			jump_timer.stop()
 		if Input.is_action_pressed("jump") and not jump_timer.is_stopped():
 			velocity.y = JUMP_VELOCITY
+			
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction = Input.get_axis("move_left", "move_right")
+		if Input.is_action_just_pressed("punch") and is_on_floor():
+			punch_timer.start(0.4)
+			direction = 0
 		if sign(direction) != sign(velocity.x):
 			emit_signal("changed_facing", sign(direction))
 		if direction:
@@ -69,6 +78,7 @@ func _physics_process(delta):
 	anim_tree.set("parameters/conditions/in_air", not is_on_floor())
 	anim_tree.set("parameters/in_air/blend_position", velocity.y - 150)
 	anim_tree.set("parameters/conditions/is_hurt", stun_timer.time_left)
+	anim_tree.set("parameters/conditions/is_punch", punch_timer.time_left)
 
 func call_kill(node, args):
 	node.kill()
@@ -82,3 +92,8 @@ func _on_changed_facing(x_value):
 
 func die():
 	get_tree().reload_current_scene()
+
+
+func _on_punch_area_body_entered(body):
+	if body != self and body.has_method("take_damage"):
+		body.take_damage(1, Vector2(scale.x*300, -300), 0.6)
--- scenes/robots/base/robot.gd
+++ scenes/robots/base/robot.gd
@@ -6,11 +6,13 @@ var direction = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var hit_points = 3
 var target_groups = ["player"]
 var target
 @onready var target_pos = global_position
 
 @onready var pause_timer = $PauseTimer
+@onready var stun_timer = $StunTimer
 @onready var derecognition = $DerecognitionRadar
 @onready var recognition = $RecognitionRadar
 @onready var damage_area = $DamageArea
@@ -19,30 +21,32 @@ var target
 @onready var emotion_anim = $EmotionAnim
 @onready var emotion = $Emotion
 @onready var anim_tree = $AnimationTree
+@onready var effect_anim = $EffectAnimationPlayer
+
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	if not pause_timer.time_left:
-		direction = 0
-		if abs(target_pos.x - global_position.x) > 5:
-				direction = sign(target_pos.x - global_position.x)
-		elif target:
-			#recognition.monitoring = false
-			damage_area.monitoring = false
-			pause_timer.start()
-			
-	else:
-		direction = 0
-	velocity.x = direction*SPEED
-	if velocity.x:
+	if not stun_timer.time_left:
+		if not pause_timer.time_left:
+			direction = 0
+			if abs(target_pos.x - global_position.x) > 5:
+					direction = sign(target_pos.x - global_position.x)
+			elif target:
+				#recognition.monitoring = false
+				damage_area.monitoring = false
+				pause_timer.start()
+				
+		elif is_on_floor():
+			direction = 0
+		velocity.x = direction*SPEED
+		if velocity.x:
 			shape.scale.x = shape.scale.y*sign(velocity.x)
 		
 	move_and_slide()
 	
-	anim_tree.set("parameters/conditions/is_idle",is_on_floor() and velocity.x == 0)
+	anim_tree.set("parameters/conditions/is_idle",(is_on_floor() and velocity.x == 0) or pause_timer.time_left)
 	anim_tree.set("parameters/conditions/is_moving", velocity.x != 0)
 	
 
@@ -81,5 +85,21 @@ func _on_damage_area_body_entered(body):
 				body.take_damage(1, Vector2(direction*300, -300), 0.5)
 			break
 
+func take_damage(dmg, kickback = -1, stun = 0):
+	hit_points -= dmg
+	if kickback is Vector2:
+		velocity = kickback
+	#effect_anim.play("hit")
+	stun_timer.start(stun)
+	#print(hit_points)
+	if hit_points <= 0:
+		die()
+	effect_anim.play("hit")
+	
+	
 func die():
 	queue_free()
+
+
+func _on_stun_timer_timeout():
+	pause_timer.start()
