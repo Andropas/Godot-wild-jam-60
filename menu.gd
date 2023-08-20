1952cfe1a276eff71521bea88eb1a59a84432fd8
PAIDEIA97 <iuri.telmo@gmail.com>
2023-08-20 03:04:29 +0100

    Added a sound effect
    Tried using autoload but the code that worked on Godot 3 doesn't work on this one

--- menu.gd
+++ menu.gd
@@ -1,6 +1,5 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MarginContainer/VBoxContainer/Start.grab_focus()
@@ -13,7 +12,9 @@ func _process(_delta):
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
+	$ButtonPress.play()
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+	$ButtonPress.play()
