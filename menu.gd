daf45360be7f5905d73450e45b83a63dcbc21fe8
PAIDEIA97 <iuri.telmo@gmail.com>
2023-08-20 02:22:08 +0100

    Menu animation added

--- menu.gd
+++ menu.gd
@@ -7,8 +7,8 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	$AnimationPlayer.play("menu")
 
 
 func _on_start_pressed():
