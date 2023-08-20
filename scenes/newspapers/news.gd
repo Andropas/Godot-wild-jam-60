60bc783617efb0904ef2e387fa426354ba939384
Andropas <mr.aagaron@gmail.com>
2023-08-20 18:37:37 +0300

    Changed newspapers behaviour

--- scenes/newspapers/news.gd
+++ scenes/newspapers/news.gd
@@ -4,26 +4,25 @@ extends Area2D
 #@export var Template: Resource
 
 @onready var template = $Template
+var shown = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_body_entered(body):
+"""func _on_body_entered(body):
 	if body.is_in_group("player"):
-		template.get_node("AnimationPlayer").play("show_news")
-		#get_parent().add_child(Template.instantiate())
-		#queue_free()
-
-
+		template.get_node("AnimationPlayer").play("show_news")"""
 
 
 func _on_body_exited(body):
 	if body.is_in_group("player"):
 		template.get_node("AnimationPlayer").play("hide_news")
+
+func _process(delta):
+	if Input.is_action_just_pressed("interact"):
+		for obj in get_overlapping_bodies():
+			if obj.is_in_group("player") and not shown:
+				template.get_node("AnimationPlayer").play("show_news")
+				shown = true
+			else: 
+				template.get_node("AnimationPlayer").play("hide_news")
+				shown = false
+
+
